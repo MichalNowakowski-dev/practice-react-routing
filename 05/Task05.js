fadb6fd2f01e3edea2273e81a73dabf73ddbddc7
mikroTesting <nowakowski.m1995@gmail.com>
2024-01-13 17:14:04 +0100

    task05 done

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,16 +1,92 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Route, useParams, useHistory, Switch } from "react-router-dom";
+import Form from "../src/components/Form";
+import productsData from "../src/products.json";
+import Shop from "../src/components/Shop";
+
+const FilterPage = ({ products = [] }) => {
+  const { searchTerm = "", minPrice, maxPrice } = useParams();
+
+  const minPriceNumber = Number(minPrice);
+  const maxPriceNumber = Number(maxPrice);
+
+  const productsAfterMinPrice = Number.isNaN(minPriceNumber)
+    ? products
+    : products.filter((product) => product.price > minPriceNumber);
+
+  const productsAfterMaxPrice = Number.isNaN(maxPriceNumber)
+    ? productsAfterMinPrice
+    : productsAfterMinPrice.filter((product) => product.price < maxPriceNumber);
+
+  const productsAfterSearchTerm =
+    searchTerm.length < 1
+      ? productsAfterMaxPrice
+      : productsAfterMaxPrice.filter(
+          (product) =>
+            product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            product.description.includes(searchTerm.toLowerCase())
+        );
+
+  if (productsAfterSearchTerm.length < 1) return <p>Brak wyników</p>;
+
+  return <Shop products={productsAfterSearchTerm} />;
+};
 
 const Task05 = () => {
-    const formFields = [
-      {
-        name: "Szukana fraza",
-        type: "text",
-      },
-    ];
-    return (
-        <h1>Task05</h1>
+  const history = useHistory();
+  const initState = {
+    minPrice: "",
+    maxPrice: "",
+    searchTerm: "",
+  };
+  const [filters, setFilters] = useState(initState);
+  const formFields = [
+    {
+      name: "Szukana fraza",
+      type: "text",
+      filterName: "searchTerm",
+    },
+    {
+      name: "Minimalna Cena",
+      type: "number",
+      filterName: "minPrice",
+    },
+    {
+      name: "Maksymalna cena",
+      type: "number",
+      filterName: "maxPrice",
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    const { minPrice, maxPrice, searchTerm } = filters;
+    e.preventDefault();
+
+    history.push(
+      `/task05/searchTerm=${searchTerm}&minPrice=${minPrice}&maxPrice=${maxPrice}`
     );
-}
+
+    setFilters(initState);
+  };
+  const handleChange = (e, filterName) => {
+    setFilters({ ...filters, [filterName]: e.target.value });
+  };
+
+  console.log(filters);
+
+  return (
+    <>
+      <Form
+        filters={filters}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        formFields={formFields}
+      />
+      <Route path="/task05/searchTerm=:searchTerm?&minPrice=:minPrice?&maxPrice=:maxPrice?">
+        <FilterPage products={productsData} />
+      </Route>
+    </>
+  );
+};
 
 export default Task05;
-
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { Route, useParams } from "react-router-dom";
 
-const Form = ({ data, formFields }) => {
-  return <form></form>;
+const Form = ({ filters, formFields, handleSubmit, handleChange }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      {formFields.map(({ name, filterName, type }) => (
+        <React.Fragment key={name}>
+          <label>
+            {name}
+            <input
+              value={filters[filterName]}
+              onChange={(e) => handleChange(e, filterName)}
+              name={name}
+              type={type}
+            ></input>
+          </label>
+          <br />
+        </React.Fragment>
+      ))}
+      <button type="submit">Szukaj</button>
+    </form>
+  );
 };
 
 export default Form;
